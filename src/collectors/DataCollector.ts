a01a83a6433565817967fff7a3fab2909cbecf82
Michal Vlasák <daeatel@gmail.com>
2020-10-01 11:56:06 +0200

    Add branches collector

--- src/collectors/DataCollector.ts
+++ src/collectors/DataCollector.ts
@@ -1,21 +1,33 @@
 import { injectable, inject } from 'inversify';
 import { ContributorsCollector } from './ContributorsCollector';
+import { BranchesCollector } from './BranchesCollector';
 import { Contributor } from '../services/git/model';
+import { ScanningStrategy } from '../detectors';
 
 @injectable()
 export class DataCollector {
   private readonly contributorsCollector: ContributorsCollector;
+  private readonly branchesCollector: BranchesCollector;
   //TODO: add tech stack collector
-  constructor(@inject(ContributorsCollector) contributorsCollector: ContributorsCollector) {
+  constructor(
+    @inject(ContributorsCollector) contributorsCollector: ContributorsCollector,
+    @inject(BranchesCollector) branchesCollector: BranchesCollector,
+  ) {
     this.contributorsCollector = contributorsCollector;
+    this.branchesCollector = branchesCollector;
   }
-  async collectData(remoteUrl: string): Promise<CollectorsData> {
+  async collectData(scanningStrategy: ScanningStrategy): Promise<CollectorsData> {
     return {
-      contributors: await this.contributorsCollector.collectData(remoteUrl),
+      contributors: await this.contributorsCollector.collectData(scanningStrategy),
+      branches: await this.branchesCollector.collectData(scanningStrategy),
     };
   }
 }
 
 export type CollectorsData = {
   contributors: Contributor[];
+  branches: {
+    default: string;
+    current: string;
+  };
 };
